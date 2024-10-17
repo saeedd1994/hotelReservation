89a67df48d283c3e0bb327e228d4513c98189a0b
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-18 00:01:57 +0330

    - generate simple routes for apps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ReservationListComponent} from "./reservation-list/reservation-list.component";
+import {ReservationFormComponent} from "./reservation-form/reservation-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'new',
+    component: ReservationFormComponent
+  },
+  {
+    path: 'list',
+    component: ReservationListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
