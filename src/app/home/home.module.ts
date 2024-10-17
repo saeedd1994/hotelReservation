e3022aa8891b20568b5fa7127457cff7a4f2044e
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-18 00:12:29 +0330

    - add routing for app

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import {RouterModule} from "@angular/router";
 
 
 
@@ -8,8 +9,9 @@ import { HomeComponent } from './home.component';
   declarations: [
     HomeComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class HomeModule { }
