a911b1868f9ffb5c272626468c015127daab0483
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-18 22:22:40 +0330

    - **** implement CRUD operations in service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Reservation} from "../models/reservation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  //CRUD
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservationById(id: string): Reservation | undefined {
+    return this.reservations.find((item) => item.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex((item) => item.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  updateReservation(updateReservation: Reservation): void {
+    let index = this.reservations.findIndex((item) => item.id === updateReservation.id);
+    this.reservations[index] = updateReservation;
+  }
 }
