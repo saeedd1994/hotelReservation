9685700e7f5aab0e6b5a9cb4ea33c8b2884e64a0
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-18 10:59:43 +0330

    - adding form Validation using fb

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.scss'
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
-  onSubmit() {
 
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.reservationForm = this.fb.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('valid')
+      console.log('this.reservationForm.value', this.reservationForm.value)
+    }
   }
 }
