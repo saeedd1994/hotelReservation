982c886f67539510af4aa137fb65559111a617be
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-25 22:50:57 +0330

    - get reservation data in list component and show it in template

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Reservation} from "../models/reservation";
+import {ReservationService} from "../reservation/reservation.service";
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.scss'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
 
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService) {
+  }
+
+  ngOnInit() {
+    this.reservations = this.reservationService.getReservations();
+  }
 }
