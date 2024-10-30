7bca13a1f75b32c8e6f03ff7dd4c81a93b12944f
saeed.da1994 <saeed.da1994@gmail.com>
2024-10-30 23:54:56 +0330

    - create id for reservation using Date.now()

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -24,6 +24,7 @@ export class ReservationService {
   }
 
   addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString();
     this.reservations.push(reservation);
     localStorage.setItem('reservations', JSON.stringify(this.reservations))
   }
